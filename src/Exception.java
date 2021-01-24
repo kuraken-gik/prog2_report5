4499c3362f0aa872f470907e6620c48a75edffd1
Kuraoka kento <e205722@ie.u-ryukyu.ac.jp>
2021-01-24 15:39:56 +0900

    1st commit

--- src/Exception.java
+++ src/Exception.java
@@ -1,6 +1,11 @@
 public class Exception{
     public static void main(String[] args){
-        String str =  null;
-        System.out.println(str.length());
+        try{
+            String str =  null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e) {
+            System.out.println("エラーが発生しました。：" + e.getMessage());
+        }
     }
 }
